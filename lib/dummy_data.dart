ff6a76efab1c56795e0450df1cab9ea90db4d7c6
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-03 21:15:42 +0530

    dummyDataCreated

---
+++ lib/dummy_data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:food/models/category_model.dart';
+
+const DUMMY_DATA = const [
+  Category(
+    id: 'a1',
+    categoryName: 'pizza',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'a2',
+    categoryName: 'Breads',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'a3',
+    categoryName: 'pasta',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'a4',
+    categoryName: 'noodles',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'a5',
+    categoryName: 'sandwich',
+    color: Colors.red,
+  ),
+];
