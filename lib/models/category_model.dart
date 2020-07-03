dfb0de3cce41d0c930d64c7913614c7f99daefb2
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-03 21:08:12 +0530

    modelCreated

---
+++ lib/models/category_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class Category {
+  final String id;
+  final String categoryName;
+  final Color color;
+
+  Category({this.id, this.categoryName, this.color});
+}
