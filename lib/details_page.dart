ad299750d3197fc74fd89139cb0f7d1bbc02d23b
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-03 23:25:01 +0530

    navigationDone

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
+  String categoryName;
+  DetailsPage(this.categoryName);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryName),
+        ),
         body: Container(
-      height: 500,
-      color: Colors.black,
-    ));
+          height: 500,
+          color: Colors.black,
+        ));
   }
 }
--- lib/widgets/category_items.dart
+++ lib/widgets/category_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food/details_page.dart';
 
 class CategoryItem extends StatelessWidget {
   final String categoryName;
@@ -7,17 +8,23 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.categoryName, this.color);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10, left: 5, right: 5),
-      child: Container(
-        child: Text(categoryName),
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(
-                colors: [color, Colors.green],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight)),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => DetailsPage(categoryName)));
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 5, right: 5),
+        child: Container(
+          child: Text(categoryName),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                  colors: [color, Colors.green],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
+        ),
       ),
     );
   }
