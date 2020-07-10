807194720fc7af33af803a57a6d71def42a5be5d
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-10 23:10:55 +0530

    ScreensOk

--- lib/favorite_screen.dart
+++ lib/favorite_screen.dart
@@ -4,9 +4,6 @@ class Favorite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('favorite'),
-      ),
       body: Center(
         child: Text('data'),
       ),
